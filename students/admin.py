048edbeeec2001e6407745ea7c7d1c73477cc7d9
PannagaBhat71 <pannagabhat886@gmail.com>
2026-09-19 03:22:20 +0530

    added admin url

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import student  # Import your model from models.py
 
+# Register the model
+admin.site.register(student)
 # Register your models here.
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('roll_number', 'student_name', 'age', 'date_created')
+    search_fields = ('roll_number', 'student_name')
+    ordering = ('-date_created',)
